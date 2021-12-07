a55125a52d4357dda8f80dfc493e319598dc30fa
Cristóbal Vargas <cristobalvg1029@gmail.com>
2021-12-06 22:58:16 -0300

    Primer commit de taller de figuras geometricas y curso de JS practico

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # curso-practico-javascript
+
+...
+
+## Taller n°1: Figuras Geométricas
+
+-Primer paso: Definir formulas
+-Segundo paso: Implementar las fórmulas en JavaScript
+-Tercer paso: Crear funciones
+-Cuarto paso: Integrar JS con HTML
---
+++ figuras.js
@@ -0,0 +1,64 @@
+// Codigo del cuadrado
+console.group("Cuadrados");
+
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+
+function areaCuadrado(lado) {
+    return  lado * lado;
+}
+
+console.groupEnd();
+
+// Codigo del triangulo
+console.group("Triangulos");
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
+
+console.groupEnd();
+
+// Codigo del circulo
+console.group("Circulos");
+
+const PI = Math.PI;
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+
+}
+
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+
+}
+
+console.groupEnd();
+
+//Interactuando con HTML
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
