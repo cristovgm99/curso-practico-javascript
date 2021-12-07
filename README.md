ee13dd925d68d21665c0a134850bd409aabdcfc8
Cristóbal Vargas <cristobalvg1029@gmail.com>
2021-12-07 15:53:29 -0300

    Commit del segundo taller

--- README.md
+++ README.md
@@ -4,7 +4,18 @@
 
 ## Taller n°1: Figuras Geométricas
 
--Primer paso: Definir formulas
+-Primer paso: Definir las formulas
 -Segundo paso: Implementar las fórmulas en JavaScript
 -Tercer paso: Crear funciones
 -Cuarto paso: Integrar JS con HTML
+
+...
+
+## Taller n°2: Porcentajes y Descuentos
+
+-Primer paso: Definir las formulas
+-Segundo paso: Implementar las fórmulas en JavaScript
+-Tercer paso: Crear funciones
+-Cuarto paso: Integrar JS con HTML
+
+...
--- figuras.js
+++ figuras.js
@@ -28,6 +28,7 @@ console.groupEnd();
 console.group("Circulos");
 
 const PI = Math.PI;
+const alCuadrado = Math.pow;
 
 function diametroCirculo(radio) {
     return radio * 2;
@@ -39,14 +40,22 @@ function perimetroCirculo(radio) {
 
 }
 
-function areaCirculo(radio) {
-    return (radio * radio) * PI;
+function areaCirculo(alCuadrado) {
+    return alCuadrado ** 2;
+}
 
+function areaCirculo2(alCuadrado) {
+    return areaCirculo(alCuadrado) * PI;
+
+}
+
+function perimetroCirculo2(diametro) {
+    return diametro * PI;
 }
 
 console.groupEnd();
 
-//Interactuando con HTML
+//Interactuando con HTML (Cuadrado)
 function calcularPerimetroCuadrado() {
     const input = document.getElementById("InputCuadrado");
     const value = input.value;
@@ -61,4 +70,41 @@ function calcularAreaCuadrado() {
 
     const area = areaCuadrado(value);
     alert(area);
+}
+
+//Interactuando con HTML (Circulo)
+function calcularPerimetroCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+
+    const perimetro = perimetroCirculo2(value);
+    alert(perimetro);
+}
+
+function calcularAreaCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+
+    const area = areaCirculo2(value);
+    alert(area);
+}
+
+//Triangulo altura
+function altura(lado1, lado2, base){
+    if(lado1 === lado2 && lado1 != base){
+        alert('isoceles')
+        const altura = Math.sqrt(lado1**2 - base**2/ 4)
+        console.log(altura)
+    }
+    else{
+        alert('no lo se')
+    }
+}
+
+function calcularAlturaTriangulo() {
+    const input = document.getElementById("InputTriangulo");
+    const value = input.value;
+
+    const alturaT = altura(value);
+    alert(alturaT);
 }
